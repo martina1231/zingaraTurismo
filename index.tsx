9137b1644e4a8775a988c2c64679f31858d09eff
martina1231 <martinavillagra21@gmail.com>
2025-12-05 17:11:42 -0300

    Delete index.tsx

--- index.tsx
+++
@@ -1,4 +0,0 @@
-// This project has been converted to a static site.
-// The entry point is now index.html which uses standard CSS and Vanilla JS.
-// React logic has been disabled to prevent "Could not find root element" errors.
-console.log('Zíngara Turismo - Static Site Mode Loaded');
